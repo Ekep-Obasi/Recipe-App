5bde679341ceaeba02c21b2ba5535a8b54b44b2f
unknown <arreyettaekep@gmail.com>
2023-07-10 19:02:14 +0100

    created the admin dashboard

---
+++ app-frontend/src/components/pages/dashboard/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Sidebar from "../../templates/sidebar/Sidebar";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <Sidebar />
+    </div>
+  );
+};
+
+export default Dashboard;
