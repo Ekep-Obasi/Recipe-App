57ee0898b3f5ef31960ce612788dcb3507ca5805
unknown <arreyettaekep@gmail.com>
2023-07-11 21:04:15 +0100

    redux store

--- app-frontend/src/app/store.js
+++ app-frontend/src/app/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import uiReducer from "./features/ui";
+import authReducer from "./features/auth";
+import userReducer from "./features/user";
 
 export default configureStore({
   reducer: {
     ui: uiReducer,
+    auth: authReducer,
+    user: userReducer,
   },
 });
