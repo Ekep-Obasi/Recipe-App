91770f5846f4f3006664421050c3b1e47cf0779a
unknown <arreyettaekep@gmail.com>
2023-07-11 21:03:27 +0100

    fetching data with axios

--- app-frontend/src/api/api.js
+++ app-frontend/src/api/api.js
@@ -4,6 +4,10 @@ export const login = (user) => httpClient.post("/login", user);
 
 export const register = (user) => httpClient.post("user", user);
 
+export const getCurrentUser = () => httpClient.get("/get-current-user");
+
+export const getAllCategories = () => httpClient.get("/category");
+
 export const forgotPassword = (data) =>
   httpClient.post("forgot-password", data);
 
