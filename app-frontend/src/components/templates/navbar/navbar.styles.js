bae4a93e1f7f348542a10a84a407093e8ba4c84d
unknown <arreyettaekep@gmail.com>
2023-07-14 23:46:27 +0100

    improved on navbar styles

--- app-frontend/src/components/templates/navbar/navbar.styles.js
+++ app-frontend/src/components/templates/navbar/navbar.styles.js
@@ -1,4 +1,4 @@
-import styled from "@emotion/styled";
+import styled from "styled-components";
 
 const StyledNav = styled.div`
   display: flex;
@@ -11,4 +11,46 @@ const StyledNav = styled.div`
   }
 `;
 
-export default StyledNav;
+const StyledHomeNav = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 25px;
+  align-items: center;
+  width: 100%;
+  padding: 0.25rem auto;
+
+  .nav-bar-row-1 {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    padding: 0 7.5%;
+    background-color: ${(props) => props.theme.color.random.rand1};
+
+    .social-media-icons {
+      display: flex;
+      gap: 10px;
+    }
+  }
+
+  .search_login {
+    display: flex;
+    gap: 12px;
+    align-items: center;
+  }
+
+  .links {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 40%;
+    justify-content: space-between;
+    position: relative;
+
+    @media screen and (max-width: 480px) {
+      width: 90%;
+    }
+  }
+`;
+
+export { StyledNav, StyledHomeNav };
