3e09168b665e2030fa80ee32d865e92db5538b5f
unknown <arreyettaekep@gmail.com>
2023-07-18 22:37:49 +0100

    dashboard styles

---
+++ app-frontend/src/components/pages/dashboard/dashboard.styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const StyledDashboard = styled.div`
+  display: flex;
+  justify-content: space-between;
+  min-height: 100vh;
+`;
+
+export { StyledDashboard };
