4bc2be1d85051f455da8df101915ab8aef286196
ARREY-ETTA BESSONG EKEP OBASI <arreyettaekep@gmail.com>
2023-11-17 20:35:30 +0100

    reafact: refactored app entry level

---
+++ server/src/index.ts
@@ -0,0 +1,18 @@
+import express, { Express, NextFunction, Request, Response } from "express";
+import startServer from "./express-app";
+import { PORT_NUMBER } from "./constants";
+import { dbTest } from "./database";
+
+(async () => {
+  const app: Express = express();
+
+  console.clear();
+
+  startServer(app);
+
+  await dbTest();
+
+  app.listen(PORT_NUMBER, () => {
+    console.log(`Server is listening on port --> ${PORT_NUMBER}            ✅`);
+  });
+})();
