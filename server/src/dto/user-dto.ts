1f47b43c3e0d71b86eba052a745b98f0794af668
ARREY-ETTA BESSONG EKEP OBASI <arreyettaekep@gmail.com>
2023-11-18 05:07:45 +0100

    feat: added more user dtos

--- server/src/dto/user-dto.ts
+++ server/src/dto/user-dto.ts
@@ -2,7 +2,10 @@ import { User } from "../domains/user";
 
 export type ISignUpUser = Omit<User, "apiKey" | "role" | "id" | "drinks">;
 
-export type ISignUpPayload = Omit<User, "apiKey" | "role" | "id" | "salt"| "drinks">;
+export type ISignUpPayload = Omit<
+  User,
+  "apiKey" | "role" | "id" | "salt" | "drinks"
+>;
 
 export type ILoginUserPayload = Pick<User, "email" | "password">;
 
