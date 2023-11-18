fa89482f25b92aa96ada6d7f541e11eee03da18c
ARREY-ETTA BESSONG EKEP OBASI <arreyettaekep@gmail.com>
2023-11-18 05:12:02 +0100

    typo: fixed typo in static filename

--- server/src/express-app.ts
+++ server/src/express-app.ts
@@ -1,13 +1,26 @@
 import express, { Express } from "express";
 import cors from "cors";
 import path from "path";
+import { CategoryRouter, userRouter } from "./routes";
+import ErrorHandler from "./helpers/errors/error-handler";
 
-const startServer = async (app: Express) => {
-
+const startServer = (app: Express) => {
   app.use(cors());
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
-  app.use(express.static(path.join(process.cwd(), "/pulic")));
+  app.use(express.static(path.join(process.cwd(), "/public")));
+
+  // routes
+  app.use("/category", CategoryRouter);
+  app.use("/user", userRouter);
+
+  // Error Handling
+  app.use(ErrorHandler);
+
+  app.on("error", (error) => {
+    console.error(error);
+    process.exit(1);
+  });
 };
 
 export default startServer;
